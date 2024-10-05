a52853ae09cfb11f6df62a6fb130050a960ca212
RedGhost1505 <joshalejandro117@gmail.com>
2024-10-05 13:27:42 -0600

    hotfix

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Theme } from '@radix-ui/themes';
 import "@radix-ui/themes/styles.css";  // Importar los estilos de Radix Themes
-import MyApp2 from './myApp2';  // Tu componente principal
+import MyApp from './myApp';  // Tu componente principal
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Theme appearance="dark">
-      <MyApp2 />
+      <MyApp />
     </Theme>
   </StrictMode>
 );
