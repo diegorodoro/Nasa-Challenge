7aceba3a8af226f51a81e66945348a1b881e8c40
RedGhost1505 <joshalejandro117@gmail.com>
2024-10-06 13:55:02 -0600

    New planets

--- src/data/detailDirectory.js
+++ src/data/detailDirectory.js
@@ -1,59 +1,81 @@
+import mars from "../assets/images/8k_mars.jpg";
+import mercury from "../assets/images/8k_mercury.jpg";
+import venus from "../assets/images/4k_venus_atmosphere.jpg";
+import earth from "../assets/images/8k_earth_daymap.jpg";
+import jupiter from "../assets/images/8k_jupiter.jpg";
+import uranus from "../assets/images/2k_uranus.jpg";
+import neptune from "../assets/images/2k_neptune.jpg";
+import saturn from "../assets/images/8k_saturn.jpg";
+
+
+
+
 const planets = [
     {
         name: "Mercury",
         diameter: "4,880 km",
         distanceFromSun: "57.91 million km",
         moons: 0,
-        description: "Mercury is the closest planet to the Sun and has no atmosphere."
+        description: "Mercury is the closest planet to the Sun and has no atmosphere.",
+        mesh: mercury,
     },
     {
         name: "Venus",
         diameter: "12,104 km",
         distanceFromSun: "108.2 million km",
         moons: 0,
-        description: "Venus is similar in structure to Earth but has a thick toxic atmosphere."
+        description: "Venus is similar in structure to Earth but has a thick toxic atmosphere.",
+        mesh: venus,
     },
     {
         name: "Earth",
         diameter: "12,742 km",
         distanceFromSun: "149.6 million km",
         moons: 1,
-        description: "Earth is the only planet known to support life."
+        description: "Earth is the only planet known to support life.",
+        mesh: earth
+
     },
     {
         name: "Mars",
         diameter: "6,779 km",
         distanceFromSun: "227.9 million km",
         moons: 2,
-        description: "The Red Planet is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury."
+        description: "The Red Planet is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.",
+        mesh: mars,
     },
     {
         name: "Jupiter",
         diameter: "139,820 km",
         distanceFromSun: "778.5 million km",
         moons: 79,
-        description: "Jupiter is the largest planet in our solar system and is a gas giant."
+        description: "Jupiter is the largest planet in our solar system and is a gas giant.",
+        mesh: jupiter,
     },
     {
         name: "Saturn",
         diameter: "116,460 km",
         distanceFromSun: "1.434 billion km",
         moons: 83,
-        description: "Saturn is known for its stunning ring system."
+        description: "Saturn is known for its stunning ring system.",
+        mesh: saturn,
+
     },
     {
         name: "Uranus",
         diameter: "50,724 km",
         distanceFromSun: "2.871 billion km",
         moons: 27,
-        description: "Uranus rotates on its side and has a faint ring system."
+        description: "Uranus rotates on its side and has a faint ring system.",
+        mesh: uranus,
     },
     {
         name: "Neptune",
         diameter: "49,244 km",
         distanceFromSun: "4.495 billion km",
         moons: 14,
-        description: "Neptune is known for its deep blue color and strong winds."
+        description: "Neptune is known for its deep blue color and strong winds.",
+        mesh: neptune,
     }
 ];
 
--- src/detailViewAlex/myApp.jsx
+++ src/detailViewAlex/myApp.jsx
@@ -4,34 +4,59 @@ import { OrbitControls, Sphere } from '@react-three/drei';
 import { TextureLoader } from 'three';
 import { Flex, Text } from "@radix-ui/themes";
 import background from "../assets/images/background.jpg";
-import planetTexture from '../assets/images/8k_mars.jpg'; // Importa la textura
+import { useGLTF } from '@react-three/drei' // Importa la textura
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { motion } from 'framer-motion';
 import planets from '../data/detailDirectory';
 import Footer from './components/footer';
 import Header from './components/header';
 
-function Planet() {
-    const texture = useLoader(TextureLoader, planetTexture);
-    const planetRef = useRef(); // Crear una referencia para el planeta
+function Planet({ planet }) {
+    const texture = useLoader(TextureLoader, planet.mesh);
+    const planetRef = useRef();
+    const [gltf, setGltf] = useState(null); // Estado para almacenar el modelo GLB
 
     // Hacer que el planeta gire lentamente
     useFrame(() => {
         if (planetRef.current) {
-            planetRef.current.rotation.y += 0.003; // Rota lentamente sobre el eje Y
+            planetRef.current.rotation.y += 0.002; // Rota lentamente sobre el eje Y
         }
     });
 
+    // Cargar el modelo GLB usando GLTFLoader
+    useEffect(() => {
+        const loader = new GLTFLoader();
+        if (planet.name === "Saturn") {
+            loader.load(
+                '../assets/images/Saturn_GLB.glb', // Asegúrate de que la ruta sea correcta
+                (gltf) => {
+                    setGltf(gltf); // Almacena el modelo en el estado
+                },
+                undefined,
+                (error) => {
+                    console.error('Error cargando el modelo:', error);
+                }
+            );
+        }
+    }, [planet.name]);
+
     return (
-        <mesh ref={planetRef}>
-            <sphereGeometry args={[2.5, 32, 32]} />
-            <meshStandardMaterial map={texture} />
-        </mesh>
+        <>
+            {planet.name === "Saturn" && gltf ? (
+                <primitive object={gltf.scene} position={[0, 0, 0]} rotation={[0, 0, 0]} />
+            ) : (
+                <mesh ref={planetRef}>
+                    <sphereGeometry args={[3, 32, 32]} />
+                    <meshStandardMaterial map={texture} />
+                </mesh>
+            )}
+        </>
     );
 }
 
 export default function MyApp() {
     const [isLoading, setIsLoading] = useState(true);
-    const mars = planets[0]; // Selecciona Marte
+    const mars = planets.find(p => p.name === "Saturn");
 
     // Simular la carga del planeta
     useEffect(() => {
@@ -57,23 +82,23 @@ export default function MyApp() {
                 <Header />
             </motion.div>
 
-            <Flex align="center" justify="center" className="flex-1 overflow-hidden"> {/* Evitar el desbordamiento */}
+            <Flex className="flex-1 overflow-hidden"> {/* Evitar el desbordamiento */}
                 {/* Contenedor de movimiento para el Canvas */}
                 <motion.div
-                    className="transform translate-x-4" // Ajusta la posición del Canvas
+                    // Ajusta la posición del Canvas
                     initial={{ opacity: 0, x: '-100%' }} // Empieza fuera de la vista
                     animate={!isLoading ? { opacity: 1, x: 0 } : {}} // Solo animar si no está cargando
                     transition={{ duration: 1.2 }}
                 >
-                    <Canvas style={{ width: '50vw', height: '100vh' }}>
-                        <OrbitControls enableZoom={true} minDistance={4.5} maxDistance={10} />
+                    <Canvas style={{ width: '65vw', height: '100%', maxWidth: "60vw", overflow: "hidden" }}>
+                        <OrbitControls enableZoom={true} minDistance={5} maxDistance={6} />
                         <ambientLight intensity={1} />
                         <pointLight position={[5, 5, 5]} intensity={1.5} />
-                        <Planet />
+                        {!isLoading && <Planet planet={mars} />}
                     </Canvas>
                 </motion.div>
 
-                <div className="w-[30%] flex flex-col p-4 overflow-hidden"> {/* Asegura disposición en columna */}
+                <div className="w-[30%] flex flex-col p-4 overflow-hidden justify-center "> {/* Asegura disposición en columna */}
                     <motion.div
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
