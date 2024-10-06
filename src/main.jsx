c8f2171c1ca7e1282d7908afb7b0c6088b99cc71
RedGhost1505 <joshalejandro117@gmail.com>
2024-10-06 16:53:43 -0600

    NewName

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Theme } from '@radix-ui/themes';
 import "@radix-ui/themes/styles.css";  // Importar los estilos de Radix Themes
-import MyApp from './detailViewAlex/myApp';  // Tu componente principal
+import Details from './detailViewAlex/DetailPlanets';  // Tu componente principal
 import "../src/index.css";  // Importar tus estilos
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Theme>
-      <MyApp />
+      <Details />
     </Theme>
   </StrictMode>
 );
