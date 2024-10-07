5d841301954f459edaf17292017c30e8d44d22df
Iñigo Quintana <quintanadinigo@gmail.com>
2024-10-06 19:21:22 -0600

    Add SolarSystem component and navigation route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import DetailView from './detailViewAlex/DetailPlanets';
 import PlanetCarousel from '../src/components/PlanetCarousel';
+import SolarSystem from './components/SolarSystem';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<PlanetCarousel />} />
         <Route path="/about" element={<DetailView />} />
+        <Route path="/orrey" element={<SolarSystem />} />
       </Routes>
     </Router>
   )
--- src/detailViewAlex/components/footer.jsx
+++ src/detailViewAlex/components/footer.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import { Flex, Text } from "@radix-ui/themes";
 import GansosNasa from '../../assets/images/Gansos-Nasa.jpg'
 import StarWarsShip from '../../assets/images/StarWarsShip.jpg'
+import { useNavigate } from 'react-router-dom';
 
 const Footer = () => {
+    const navigate = useNavigate()
+    
     return (
         <>
             <Flex
@@ -12,7 +15,8 @@ const Footer = () => {
                 justify="end"
                 className="font-sans h-16 "
             >
-                <img src={StarWarsShip} alt="Star Wars Ship" className="h-16 w-16 mb-6" />
+                
+                <img onClick={()=>navigate('/orrey')} src={StarWarsShip} alt="Star Wars Ship" className="h-16 w-16 mb-6" />
                 <img src={GansosNasa} alt="Gansos Nasa" className="h-16 w-16 m-6" />
 
             </Flex>
